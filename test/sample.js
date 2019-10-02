520baf952a7f8e2ef0d2bd29d48612e67abb8957
Luka Bulatovic <luka.dev94@gmail.com>
2019-10-02 23:28:17 +0200

    first commit

---
+++ test/sample.js
@@ -0,0 +1,9 @@
+var assert = require("assert");
+
+describe("Something", function() {
+    describe("How it works", function() {
+        it("is pungent", function() {
+            assert.ok(true);
+        });
+    });
+});
